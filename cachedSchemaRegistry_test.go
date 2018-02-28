66d9b6f1bb5e4f070d05fc6fc32a57eeb6b0df91
Dang Nguyen <hoangdang.me@gmail.com>
2018-02-28 18:07:28 +0700

    schema registry unittest

---
+++ cachedSchemaRegistry_test.go
@@ -0,0 +1,142 @@
+package kafka
+
+import (
+	"testing"
+)
+
+func TestCachedSchemaRegistryClient_GetSchema(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	client.GetSchema(1)
+	responseCodec, err := client.GetSchema(1)
+	if nil != err {
+		t.Errorf("Error getting schema: %s", err.Error())
+	}
+	if responseCodec.Schema() != testObject.Codec.Schema() {
+		t.Errorf("Schemas do not match. Expected: %s, got: %s", testObject.Codec.Schema(), responseCodec.Schema())
+	}
+	if testObject.Count > 1 {
+		t.Errorf("Expected call count of 1, got %d", testObject.Count)
+	}
+}
+
+func TestCachedSchemaRegistryClient_GetSubjects(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	subjects, err := client.GetSubjects()
+	if nil != err {
+		t.Errorf("Error getting subjects: %v", err)
+	}
+	if !containsStr(subjects, testObject.Subject) {
+		t.Errorf("Could not find subject")
+	}
+}
+
+func TestCachedSchemaRegistryClient_GetVersions(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	versions, err := client.GetVersions(testObject.Subject)
+	if nil != err {
+		t.Errorf("Error getting versions: %v", err)
+	}
+	if !containsInt(versions, testObject.Id) {
+		t.Errorf("Could not find version")
+	}
+}
+
+func TestCachedSchemaRegistryClient_GetSchemaByVersion(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	responseCodec, err := client.GetSchemaByVersion(testObject.Subject, 1)
+	if nil != err {
+		t.Errorf("Error getting schema versions: %v", err)
+	}
+	if responseCodec.Schema() != testObject.Codec.Schema() {
+		t.Errorf("Schemas do not match. Expected: %s, got: %s", testObject.Codec.Schema(), responseCodec.Schema())
+	}
+}
+
+func TestCachedSchemaRegistryClient_GetLatestSchema(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	responseCodec, err := client.GetLatestSchema(testObject.Subject)
+	if nil != err {
+		t.Errorf("Error getting latest schema: %v", err)
+	}
+	if responseCodec.Schema() != testObject.Codec.Schema() {
+		t.Errorf("Schemas do not match. Expected: %s, got: %s", testObject.Codec.Schema(), responseCodec.Schema())
+	}
+}
+
+func TestCachedSchemaRegistryClient_CreateSubject(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	id, err := client.CreateSubject(testObject.Subject, testObject.Codec)
+	if nil != err {
+		t.Errorf("Error getting schema: %s", err.Error())
+	}
+	if id != testObject.Id {
+		t.Errorf("Ids do not match. Expected: 1, got: %s", id)
+	}
+	sameid, err := client.CreateSubject(testObject.Subject, testObject.Codec)
+	if nil != err {
+		t.Errorf("Error getting schema: %s", err.Error())
+	}
+	if sameid != id {
+		t.Errorf("Ids do not match. Expected: %s, got: %s", id, sameid)
+	}
+	if testObject.Count > 1 {
+		t.Errorf("Expected call count of 1, got %d", testObject.Count)
+	}
+}
+
+func TestCachedSchemaRegistryClient_IsSchemaRegistered(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	id, err := client.IsSchemaRegistered(testObject.Subject, testObject.Codec)
+	if nil != err {
+		t.Errorf("Error getting schema id: %v", err)
+	}
+	if nil != err {
+		t.Errorf("Error getting schema: %s", err.Error())
+	}
+	if id != testObject.Id {
+		t.Errorf("Ids do not match. Expected: 1, got: %s", id)
+	}
+}
+
+func TestCachedSchemaRegistryClient_DeleteSubject(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	err := client.DeleteSubject(testObject.Subject)
+	if nil != err {
+		t.Errorf("Error delete subject: %v", err)
+	}
+}
+
+func TestCachedSchemaRegistryClient_DeleteVersion(t *testing.T) {
+	testObject := createSchemaRegistryTestObject(t, "test", 1)
+	mockServer := testObject.MockServer
+	defer mockServer.Close()
+	client := NewCachedSchemaRegistryClient([]string{mockServer.URL})
+	err := client.DeleteVersion(testObject.Subject, 1)
+	if nil != err {
+		t.Errorf("Error delete version: %v", err)
+	}
+}
---
+++ schemaRegistry_test.go
@@ -0,0 +1,135 @@
+package kafka
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/linkedin/goavro"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type TestObject struct {
+	MockServer *httptest.Server
+	Codec      *goavro.Codec
+	Subject    string
+	Id         int
+	Count      int
+}
+
+func createSchemaRegistryTestObject(t *testing.T, subject string, id int) *TestObject {
+	testObject := &TestObject{}
+	testObject.Subject = subject
+	testObject.Id = id
+	testObject.Count = 0
+	codec, err := goavro.NewCodec(`
+        {
+          "type": "record",
+          "name": "test",
+          "fields" : [
+            {"name": "val", "type": "int", "default": 0}
+          ]
+        }`)
+	if err != nil {
+		t.Errorf("Could not create codec %v", err)
+	}
+	testObject.Codec = codec
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		testObject.Count++
+		if r.Method == "POST" {
+			switch r.URL.String() {
+			case fmt.Sprintf(subjectVersions, subject), fmt.Sprintf(deleteSubject, subject):
+				response := idResponse{id}
+				str, _ := json.Marshal(response)
+				fmt.Fprintf(w, string(str))
+			}
+		} else if r.Method == "GET" {
+			switch r.URL.String() {
+			case fmt.Sprintf(schemaByID, id):
+				escapedSchema := strings.Replace(codec.Schema(), "\"", "\\\"", -1)
+				fmt.Fprintf(w, `{"schema": "%s"}`, escapedSchema)
+			case subjects:
+				response := []string{subject}
+				str, _ := json.Marshal(response)
+				fmt.Fprintf(w, string(str))
+			case fmt.Sprintf(subjectVersions, subject):
+				response := []int{id}
+				str, _ := json.Marshal(response)
+				fmt.Fprintf(w, string(str))
+			case fmt.Sprintf(subjectByVersion, subject, "1"), fmt.Sprintf(subjectByVersion, subject, "latest"):
+				response := schemaVersionResponse{subject, 1, codec.Schema(), id}
+				str, _ := json.Marshal(response)
+				fmt.Fprintf(w, string(str))
+			}
+		} else if r.Method == "DELETE" {
+			switch r.URL.String() {
+			case fmt.Sprintf(deleteSubject, subject),
+				fmt.Sprintf(subjectByVersion, subject, fmt.Sprintf("%d", 1)):
+				fmt.Fprintf(w, "1")
+			}
+		}
+
+	}))
+	testObject.MockServer = server
+	return testObject
+}
+
+func containsStr(array []string, value string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func containsInt(array []int, value int) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func TestSchemaRegistryClient_Retries(t *testing.T) {
+	count := 0
+	response := []string{"test"}
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		count++
+		w.Header().Set("Content-Type", contentType)
+		if count < 3 {
+			http.Error(w, `{"error_code": 500, "message": "Error in the backend datastore"}`, 500)
+		} else {
+			str, _ := json.Marshal(response)
+			fmt.Fprintf(w, string(str))
+		}
+	}))
+	SchemaRegistryClient := NewSchemaRegistryClientWithRetries([]string{mockServer.URL}, 2)
+	subjects, err := SchemaRegistryClient.GetSubjects()
+	if err != nil {
+		t.Errorf("Found error %s", err)
+	}
+	if !reflect.DeepEqual(subjects, response) {
+		t.Errorf("Subjects did not match expected %s, got %s", response, subjects)
+	}
+	expectedCallCount := 3
+	if count != expectedCallCount {
+		t.Errorf("Expected error count to be %d, got %d", expectedCallCount, count)
+	}
+}
+
+func TestSchemaRegistryClient_Error(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, `{"error_code": 500, "message": "Error in the backend datastore"}`, 500)
+	}))
+	SchemaRegistryClient := NewSchemaRegistryClient([]string{mockServer.URL})
+	_, err := SchemaRegistryClient.GetSubjects()
+	expectedErr := Error{500, "Error in the backend datastore"}
+	if err.Error() != expectedErr.Error() {
+		t.Errorf("Expected error to be %s, got %s", expectedErr.Error(), err.Error())
+	}
+}
